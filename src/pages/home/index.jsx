10bc3a873bd83c22a7f777ced3780ffc009e4bc0
Iarley-01 <yarleyalves2016@gmail.com>
2024-07-18 14:04:15 -0300

    usando a api para pegar informações dos filmes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
+const urlApi = import.meta.env.VITE_API;
+const urlKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+  
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    console.log(data);
+  }
+  
+  useEffect(() => {
+    const topRatedUrl = `${urlApi}top_rated?${urlKey}`
+    getTopRatedMovies(topRatedUrl)
+  }, [])
   return (
     <h2>HomePage</h2>
   )
